cbba92075c397a0e45640ca495e10ecde61fcda4
Marius Storhaug <marstor@hotmail.com>
2025-01-22 15:45:21 +0100

    Replace main.sh with main.ps1 and enhance PowerShell script with error handling and debug logging

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,7 +1,24 @@
+#Requires -Modules GitHub
+
 [CmdletBinding()]
 param(
     [Parameter()]
     [string] $Subject = $env:GITHUB_ACTION_INPUT_subject
 )
 
-Write-Host "Hello, $subject!"
+begin {
+    $scriptName = $MyInvocation.MyCommand.Name
+    Write-Debug "[$scriptName] - Start"
+}
+
+process {
+    try {
+        Write-Output "Hello, $Subject!"
+    } catch {
+        throw $_
+    }
+}
+
+end {
+    Write-Debug "[$scriptName] - End"
+}
--- scripts/main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run 'git add --chmod=+x scripts\main.sh' to make the script executable
-
-echo "Hello, $SUBJECT!"
